5e4b6837a8a52af164e8a1ebe47084b2318e251a
`Enrique` <`e.jimenezmoreno@gmail.com`>
2023-02-24 15:36:18 +0100

    Add createRobotController

--- src/api/robots/robots-controllers.ts
+++ src/api/robots/robots-controllers.ts
@@ -1,10 +1,26 @@
+import crypto from 'node:crypto';
 import { RequestHandler } from 'express';
 import { RobotModel } from './robots-schema.js';
+import { robotSchema } from './robots-schema.js';
 
 export const getRobotsController: RequestHandler = async (_req, res) => {
   try {
     const foundRobots = await RobotModel.find({});
     res.json(foundRobots);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+export const createRobotController: RequestHandler = async (req, res) => {
+  const id = crypto.randomUUID();
+  const robot: typeof robotSchema = {
+    id,
+    ...req.body,
+  };
+  try {
+    await RobotModel.create(robot);
+    res.status(201).json(robot);
   } catch (error) {
     res.status(500).json(error);
   }
