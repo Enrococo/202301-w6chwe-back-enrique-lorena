bbd81a0d80ac5542c1470accc6caabc36589496d
LorenaAnayaDiaz <lorena.anaya.diaz@gmail.com>
2023-02-24 17:45:39 +0100

    add get by id route

--- src/api/robots/robots-router.ts
+++ src/api/robots/robots-router.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import {
   createRobotController,
+  getRobotByIdController,
   getRobotsController,
 } from './robots-controllers.js';
 
@@ -8,6 +9,6 @@ const router = express.Router();
 
 router.route('/').get(getRobotsController).post(createRobotController);
 
-router.route('/:id');
+router.route('/:id').get(getRobotByIdController);
 
 export default router;
