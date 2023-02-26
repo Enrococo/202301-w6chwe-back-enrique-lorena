c246e635d05d84ee42c6483812cf7bf4ba907426
LorenaAnayaDiaz <lorena.anaya.diaz@gmail.com>
2023-02-26 14:19:54 +0100

    add cors to server and install cors dependencies

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,12 @@ dotenv.config();
 import app from './app.js';
 import connectDB from './database/connection.js';
 import log from './logger.js';
+import cors from 'cors';
 
 const port = process.env.PORT ?? 3000;
 const mongoUrl = process.env.MONGO_DB ?? '';
+
+app.use(cors());
 
 app.listen(port, async () => {
   await connectDB(mongoUrl);
